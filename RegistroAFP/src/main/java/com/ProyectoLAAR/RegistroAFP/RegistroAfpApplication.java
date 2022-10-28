2396c2871824ce08645eb2c30f1509ed14c24922
Luis Acuña <luisandresverdugox@gmail.com>
2022-10-27 23:24:12 -0500

    Subiendo código de microservicios para REGISTRO AFP

---
+++ RegistroAFP/src/main/java/com/ProyectoLAAR/RegistroAFP/RegistroAfpApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoLAAR.RegistroAFP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroAfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroAfpApplication.class, args);
+	}
+
+}
---
+++ RegistroAFP/src/test/java/com/ProyectoLAAR/RegistroAFP/RegistroAfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoLAAR.RegistroAFP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroAfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
